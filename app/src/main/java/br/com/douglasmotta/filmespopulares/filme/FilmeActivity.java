edd0b92d881816721d25c1bf267e2732b7b1dcda
Douglas Motta <douglas1102rm@hotmail.com>
2018-04-16 20:56:37 -0300

    Aula 02

--- app/src/main/java/br/com/douglasmotta/filmespopulares/filme/FilmeActivity.java
+++ app/src/main/java/br/com/douglasmotta/filmespopulares/filme/FilmeActivity.java
@@ -1,16 +1,20 @@
 package br.com.douglasmotta.filmespopulares.filme;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import br.com.douglasmotta.filmespopulares.R;
 
-public class FilmeActivity extends Activity {
+public class FilmeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_filme);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
